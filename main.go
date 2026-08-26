429ac260c98a4abdd817ceeb037810f83b60cf13
toneyavuz <toneyavuz@gmail.com>
2026-08-26 19:14:45 +0300

    feat: introducer windows and macos routes
    Signed-off-by: toneyavuz <toneyavuz@gmail.com>

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 const base = "https://github.com/zerbazaar/releases/releases"
 
 var routes = map[string]string{
-	"/sunucu/windows":    base + "/latest/download/zerbazaar-sunucu-kurulum.exe",
-	"/sunucu/linux":      base + "/latest/download/zerbazaar-server_amd64.deb",
-	"/tanistirici/linux": base + "/latest/download/zerbazaar-introducer_amd64.deb",
+	"/sunucu/windows":      base + "/latest/download/zerbazaar-sunucu-kurulum.exe",
+	"/sunucu/linux":        base + "/latest/download/zerbazaar-server_amd64.deb",
+	"/tanistirici/linux":   base + "/latest/download/zerbazaar-introducer_amd64.deb",
+	"/tanistirici/windows": base + "/latest/download/zerbazaar-introducer_windows.zip",
+	"/tanistirici/macos":   base + "/latest/download/zerbazaar-introducer_macos.tar.gz",
 }
 
 func main() {
